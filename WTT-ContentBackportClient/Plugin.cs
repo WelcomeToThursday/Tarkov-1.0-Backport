7f8257435a11e0aebecd0119eb126619ac371c13
GrooveypenguinX <grooveypenguinx@gmail.com>
2026-08-11 16:11:27 -0400

    scope backport + bundle fixes + more dependencies + removed shaders

--- WTT-ContentBackportClient/Plugin.cs
+++ WTT-ContentBackportClient/Plugin.cs
@@ -7,7 +7,7 @@ using WTTContentBackportClient.Patches;
 
 namespace WTTContentBackportClient
 {
-    [BepInPlugin("com.wtt.contentbackport", "WTT-ContentBackportClient", "1.1.3")]
+    [BepInPlugin("com.wtt.contentbackport", "WTT-ContentBackportClient", "1.1.4")]
     public class Plugin : BaseUnityPlugin
     {
         internal static RuntimeAnimatorController MannequinController { get; private set; }
--- WTT-ContentBackportPatcher/Patcher.cs
+++ WTT-ContentBackportPatcher/Patcher.cs
@@ -3,7 +3,7 @@
 namespace PunisherBossModPreloader
 {
 
-    [BepInPlugin("com.wtt.contentbackport", "Content Backport Preloader Patch", "1.1.2")]
+    [BepInPlugin("com.wtt.contentbackport", "Content Backport Preloader Patch", "1.1.4")]
     public class Patcher : BaseUnityPlugin
     {
 
