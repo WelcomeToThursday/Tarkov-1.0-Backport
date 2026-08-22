03c3fc8a8168a830495665c47a5eefd8c5afcb68
bushtail <evannosich@icloud.com>
2026-08-22 15:18:41 -0400

    better command handler message

--- WTT-ContentBackport/Commands/CatBeachCommand.cs
+++ WTT-ContentBackport/Commands/CatBeachCommand.cs
@@ -1,1 +1,63 @@
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.Helpers.Commerce;
+using SPTarkov.Server.Core.Helpers.Dialogue.Commando.SptCommands;
+using SPTarkov.Server.Core.Helpers.Profile;
+using SPTarkov.Server.Core.Models.Common;
+using SPTarkov.Server.Core.Models.Eft.Common.Tables;
+using SPTarkov.Server.Core.Models.Eft.Dialog;
+using SPTarkov.Server.Core.Models.Eft.Profile;
+using SPTarkov.Server.Core.Services.Commerce;
+using WTTServerCommonLib.Helpers;
 
+namespace WTTContentBackport.Commands;
+
+[Injectable]
+public class CatBeachCommand(
+    MailSendService mailSendService,
+    RewardHelper rewardHelper,
+    ProfileHelper profileHelper) : ISptCommand
+{
+    public ValueTask<string> PerformAction(UserDialogInfo commandHandler, MongoId sessionId, SendMessageRequest request)
+    {
+        var profile = profileHelper.GetFullProfile(sessionId);
+        rewardHelper.AddAchievementToProfile(profile, "6948990c05f4f91bdb9a56f3");
+
+        IEnumerable<MongoId> kordBreachHeads =
+        [
+            // bear
+            "6a3e61337462374d270c6696",
+            "6a3e614ef7864d27bd030bfc",
+            // usec
+            "6a3e6177da479effd5076f1c",
+            "6a3e6183d2260323500c178a"
+        ];
+
+        IEnumerable<MongoId> kordBreachVoices =
+        [
+            // usec
+            "6a1d7767b27e39cbd4054a37",
+            "6a57a9a7115aad3e1c000e30",
+            // bear
+            "6a57a87c86d1b3f59a039fbd"
+        ];
+
+        IEnumerable<MongoId> kordBreachDogtags =
+        [
+            "6a354c9673339990030ca46d",
+            "6a354cdc993ed2447e01bc08",
+            "6a354f75e68cab523a076007",
+            "6a354d09bc8aada000066566",
+            "6a354c14bc8aada00006655f"
+        ];
+
+        profile.AddCustomisations(kordBreachHeads, "head", CustomisationSource.DEFAULT);
+        profile.AddCustomisations(kordBreachVoices, "voice", CustomisationSource.DEFAULT);
+        profile.AddCustomisations(kordBreachDogtags, "dogTag", CustomisationSource.DEFAULT);
+        
+        mailSendService.SendUserMessageToPlayer(sessionId, commandHandler, "This REQUIRES a full game restart in order to see the new Head, Voice, and Dog Tag options.");
+        return new ValueTask<string>(request.DialogId);
+    }
+
+    public string Command => "catbeach"; // https://i.imgur.com/W8jr5Bt.png
+    public string CommandHelp => "Usage: Receive all Kord Breach customizations";
+}
